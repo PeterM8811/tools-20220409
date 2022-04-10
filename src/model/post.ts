3319b591952d1d53bee66db653b31d1c0752cb41
PeterM8811 <pmlynarski88@gmail.com>
2022-04-10 13:48:33 +0200

    ekspot import 2

--- src/model/post.ts
+++ src/model/post.ts
@@ -5,17 +5,5 @@ export interface Post {
     body: string;
 };
 
-export interface Author {
-    id: number;
-    name: string;
-    username: string;
-    email: string;
-};
 
-export interface Comment {
-    postId: number;
-    id: number;
-    name: string;
-    email: string;
-    body: string;
-}
+
